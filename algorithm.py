8fe9ce29fac9381b377ec5486f38825559da12e3
Aram-Hov <61308460+Aram-Hov@users.noreply.github.com>
2020-03-21 20:52:55 +0400

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,22 +1,28 @@
-distance = int(input("Distance covered - "))
-fuel_initial = float(input("Amount of fuel before the start of the trip - "))
-fuel_left = float(input("Amount of fuel left - "))
-fuel_consumed = fuel_initial - fuel_left
-consumption_1 = distance / fuel_consumed
-consumption_1 = round(consumption_1, 1)
-consumption_2 = round(100 / consumption_1, 1)
+import time
 
-choice = input("Choose either km/L or L/100km ")
-car_on = True
- while True:
-   if choice == "km/L":
-    print(consumption_1, "kilometers per liter")
+def execute_something():
+    choice = input("Choose either km/L or L/100km ")
+    if choice == "km/L":
+        print(consumption_1, "kilometers per liter")
 
-   elif choice == "L/100km":
-    print(consumption_2, "L/100km")
-   else:
-    print("Wrong input!")
-   break
+    elif choice == "L/100km":
+        print(consumption_2, "L/100km")
+    else:
+        print("Wrong input!")
+    time.sleep(20)
+
+while True:
+    distance = float(input("Distance covered - "))
+    fuel_initial = float(input("Amount of fuel before the start of the trip - "))
+    fuel_left = float(input("Amount of fuel left - "))
+
+    fuel_consumed = fuel_initial - fuel_left
+    consumption_1 = distance / fuel_consumed
+    consumption_1 = round(consumption_1, 1)
+    consumption_2 = round(100 / consumption_1, 1)
+
+    execute_something()
+
 
 
 
