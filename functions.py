465febe67af38a78c1cc999c6c8ec87b7daf8cc3
Aram-Hov <61308460+Aram-Hov@users.noreply.github.com>
2020-05-03 17:58:14 +0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,47 +2,52 @@ import json
 import time
 
 
-def load_vehicle_lists():
+def load_vehicle_data():
     with open("data.JSON") as f:
         data = json.load(f)
     return data
 
 
+
+
 def choose_story():
     while True:
-        story = input("Press 1 for Trip simulation \nPress 2 for brand/model search \nPress 3 to look up in specific range \n>>>")
-        if story not in ('1', '2', '3',):
+        story = input("Press 1 for Trip simulation \nPress 2 for brand/model search \nPress 3 to look up in specific "
+                      "range \nPress 4 to search for a vehicle \n>>> ")
+        if story not in ('1', '2', '3', '4'):
             print("Not an appropriate choice. Try again!")
         else:
             break
 
     return story
 
+def calculations_for_specific_vehicles(fuel_consumed):
+    distance = 0.3
+    consumption_km_per_L = distance / fuel_consumed
+    consumption_km_per_L = round(consumption_km_per_L, 1)
+    consumption_L_per_100km = round(100 / consumption_km_per_L, 1)
+    return consumption_L_per_100km
 
-def simulation():
-    data = load_vehicle_lists()
+
+
+def simulate_real_time_consumption(data):
     for sample in data["trip_simulating_values"]:
         distance = 0.3
-        consumption_1 = distance / sample
-        consumption_1 = round(consumption_1, 1)
-        consumption_2 = round(100 / consumption_1, 1)
-        print(consumption_2, "L/100km")
+        consumption_km_per_L = distance / sample
+        consumption_km_per_L = round(consumption_km_per_L, 1)
+        consumption_L_per_100km = round(100 / consumption_km_per_L, 1)
+        print(consumption_L_per_100km, "L/100km")
         time.sleep(1)
-    print("Trip is over, goodbye!")
+    print("The trip is over, have a nice day!")
 
 
-def vehicle_list():
-    data = load_vehicle_lists()
+def print_vehicle_list(data):
     for vehicle in data["vehicle_brands"]:
-        distance = 0.3
-        fuel_consumed = vehicle["fuel for 0.3km"]
-        consumption_1 = distance / fuel_consumed
-        consumption_1 = round(consumption_1, 1)
-        consumption_2 = round(100 / consumption_1, 1)
+        consumption_L_per_100km = calculations_for_specific_vehicles(vehicle["fuel for 0.3km"])
+        print("\n\n", vehicle["brand"], vehicle["series"], "consumes", consumption_L_per_100km, "L/100km", end="")
 
-        print("\n\n", vehicle["brand"], vehicle["series"], "consumes", consumption_2, "L/100km", end="")
 
-def vehicle_list_range_search():
+def vehicle_list_print_in_range(data):
     while True:
         while True:
             lower_bound = input('\nEnter lowest bound you are looking for > ')
@@ -54,35 +59,72 @@ def vehicle_list_range_search():
         while True:
             upper_bound = input('\nEnter the upper bound you are looking for > ')
             if upper_bound.isnumeric():
-                upper_bound = float(lower_bound)
+                upper_bound = float(upper_bound)
                 break
+
+            elif float(lower_bound) >= float(upper_bound):
+                print("lower bound can not be greater than upper bound, please try again!")
+
             else:
-                print("\nYour input is not numeric, please try again!")
-        if lower_bound >= upper_bound:
-            print("lower bound can not be greater than upper bound, please try again!")
+                print("\nYour input is not numeric or is negative, please try again!")
+
+                break
+        counter = 0
+        for vehicle in data["vehicle_brands"]:
+            consumption_L_per_100km = calculations_for_specific_vehicles(vehicle["fuel for 0.3km"])
+            if lower_bound < consumption_L_per_100km < upper_bound :
+                print("\n\n", vehicle["brand"], vehicle["series"], "consumes", consumption_L_per_100km, "L/100km", end="")
+                counter += 1
+        if counter == 0 :
+            print("No vehicle was found in the given range, try again!")
+        else:
+            print("\n\nOverall", "{} matches found.".format(counter))
+            break
+
+
+def vehicle_lookup():
+
+    data = load_vehicle_data()
+
+    brand_found = False
+    series_found = False
+
+    while True:
+        if (brand_found == False):
+            brand = input("Insert the brand of the vehicle you are looking for > ")
+            brand = brand.lower()
+
+        series = input("Insert the model of the vehicle you are looking for > ")
+        series = series.lower()
+
+        for vehicle in data["vehicle_brands"]:
+
+            if vehicle['brand'] == brand:
+                brand_found = True
+                if vehicle['series'] == series:
+                    series_found = True
+                    consumption_L_per_100km = calculations_for_specific_vehicles(vehicle["fuel for 0.3km"])
+
+                    print(brand, series, "consumes", consumption_L_per_100km, "L/100km")
+                    break
+
+        if brand_found == False:
+            print("the brand", brand, "was not found")
+
+        elif series_found == False:
+            print("the series", series, "not found")
         else:
             break
 
 
-    data = load_vehicle_lists()
-    for vehicle in data["vehicle_brands"]:
-        distance = 0.3
-        fuel_consumed = vehicle["fuel for 0.3km"]
-        consumption_1 = distance / fuel_consumed
-        consumption_1 = round(consumption_1, 1)
-        consumption_2 = round(100 / consumption_1, 1)
-        if lower_bound < consumption_2 < upper_bound :
-            print("\n\n", vehicle["brand"], vehicle["series"], "consumes", consumption_2, "L/100km", end="")
-
-
-
-
-
-def choice_of_story(story):
+def execution_of_choosen_feature(story):
+    data = load_vehicle_data()
     if story == "1":
-        simulation()
+        simulate_real_time_consumption(data)
     elif story == "2":
-        vehicle_list()
+        print_vehicle_list(data)
     elif story == "3":
-        vehicle_list_range_search()
+        vehicle_list_print_in_range(data)
+    elif story == "4":
+        vehicle_lookup()
 
